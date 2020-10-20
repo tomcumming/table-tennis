84a2c9f994d8ee640f4acfe5000dc6d05e715818
Tom Cumming <mail@tomcumming.co.uk>
2020-10-20 20:14:21 +0100

    polynomial division

--- src/sim/polynomial.test.ts
+++ src/sim/polynomial.test.ts
@@ -1,5 +1,6 @@
 import { assert } from "https://deno.land/std@0.74.0/testing/asserts.ts";
-import { derivative, Polynomial, roughlyEq } from "./polynomial.ts";
+import { derivative, Polynomial, roughlyEq, solveRoot } from "./polynomial.ts";
+import { roughlyEq as scalarRE } from "../math/scalar.ts";
 
 Deno.test("Derivative of 3x^2 + 2x^2 + 4x + -1", () => {
   const original: Polynomial = [-1, 4, 2, 3];
@@ -9,4 +10,26 @@ Deno.test("Derivative of 3x^2 + 2x^2 + 4x + -1", () => {
     roughlyEq(expected, result),
     `${expected} ~= ${result}`,
   );
+});
+
+Deno.test("Solve (x^3 - 2x^2 - 5x + 6) when a root is x = 3", () => {
+  const original: Polynomial = [6, -5, -2, 1];
+  const expected: Polynomial = [-2, 1, 1];
+  const [result, remainder] = solveRoot(original, 3);
+  assert(
+    roughlyEq(expected, result),
+    `${expected} ~= ${result}`,
+  );
+  assert(scalarRE(remainder, 0), `${remainder} ~= 0`);
+});
+
+Deno.test("Solve (x^3 + 5x^2 + 7x + 2) when a root is x = -2", () => {
+  const original: Polynomial = [2, 7, 5, 1];
+  const expected: Polynomial = [1, 3, 1];
+  const [result, remainder] = solveRoot(original, -2);
+  assert(
+    roughlyEq(expected, result),
+    `${expected} ~= ${result}`,
+  );
+  assert(scalarRE(remainder, 0), `${remainder} ~= 0`);
 });
--- src/sim/polynomial.ts
+++ src/sim/polynomial.ts
@@ -3,6 +3,8 @@ import { roughlyEq as scalarRE } from "../math/scalar.ts";
 /** [a, b, c...] = a + bx + cx^2 ... */
 export type Polynomial = number[];
 
+const POLY_TOO_SMALL = `Polynomial order is too low`;
+
 export function roughlyEq(
   a: Polynomial,
   b: Polynomial,
@@ -13,4 +15,20 @@ export function roughlyEq(
 
 export function derivative(poly: Polynomial): Polynomial {
   return poly.slice(1).map((a, i) => a * (i + 1));
+}
+
+/** @returns The result and a remainder */
+export function solveRoot(
+  poly: Polynomial,
+  x: number,
+): [Polynomial, number] {
+  if (poly.length < 2) throw new Error(POLY_TOO_SMALL);
+  const divided = poly
+    .slice()
+    .reverse()
+    .reduce(
+      (rest, c) => rest.length === 0 ? [c] : [x * rest[0] + c, ...rest],
+      [] as number[],
+    );
+  return [divided.slice(1), divided[0]];
 }
