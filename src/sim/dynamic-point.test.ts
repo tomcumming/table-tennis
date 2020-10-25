5ab48b5f46954a57d65fa1f90816fbb332abc88b
Tom Cumming <mail@tomcumming.co.uk>
2020-10-25 17:03:06 +0000

    More tests for time to distance

--- src/sim/dynamic-point.test.ts
+++ src/sim/dynamic-point.test.ts
@@ -5,6 +5,7 @@ import { BALL_RADIUS, FLOOR, GRAVITY } from "./constants.ts";
 import { EXPECTED_SOLUTION } from "./phys.test.ts";
 import { roughlyEq } from "../math/scalar.ts";
 import * as v2 from "../math/v2.ts";
+import { quadratic } from "../math/polynomial.ts";
 
 Deno.test("DynamicPoint straight up to floor", () => {
   const startSpeed = 20;
@@ -60,6 +61,59 @@ Deno.test("DynamicPoint up and down to circle below", () => {
     { ...ball, pos: v2.sub(bat, ball.pos) },
     GRAVITY,
   );
+  assert(
+    times.some((t) => roughlyEq(t, expectedTime)),
+    `${expectedTime} in ${JSON.stringify(times)}`,
+  );
+});
+
+Deno.test("DynamicPoint drop to static circle below", () => {
+  const startHeight = 10;
+
+  const ball: DynamicPoint = {
+    pos: [1, startHeight],
+    vel: v2.ZERO,
+  };
+
+  const r = 2;
+  const hitHeight = Math.sqrt(r ** 2 - 1 ** 2);
+
+  // (sh - hh) + g t^2 0.5 = 0
+  const expectedTime = Math.sqrt(
+    -(startHeight - hitHeight) / (GRAVITY[1] * 0.5),
+  );
+  console.log({ expectedTime });
+
+  const times = timeToDistance(r, ball, GRAVITY);
+  assert(
+    times.some((t) => roughlyEq(t, expectedTime)),
+    `${expectedTime} in ${JSON.stringify(times)}`,
+  );
+});
+
+Deno.test("DynamicPoint drop to moving circle below", () => {
+  const startHeight = 10;
+
+  const ball: DynamicPoint = {
+    pos: [1, startHeight],
+    vel: v2.ZERO,
+  };
+  const batVel: v2.V2 = [0, 3];
+
+  const r = 2;
+  const hitHeight = Math.sqrt(r ** 2 - 1 ** 2);
+  const relativeBall: DynamicPoint = {
+    pos: ball.pos,
+    vel: v2.sub(ball.vel, batVel),
+  };
+
+  // (sh - hh) + 3t + g t^2 0.5 = 0
+  const [expectedTime] = quadratic(
+    [startHeight - hitHeight, relativeBall.vel[1], GRAVITY[1] * 0.5],
+  )
+    .filter((t) => t > 0);
+
+  const times = timeToDistance(r, relativeBall, GRAVITY);
   assert(
     times.some((t) => roughlyEq(t, expectedTime)),
     `${expectedTime} in ${JSON.stringify(times)}`,
